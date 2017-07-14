58067951aff4430e85e72a65613aca0fd3748a28
caicai <594665291@qq.com>
2017-07-14 22:14:54 +0800

    修改错误注释

--- src/com/kongzhu/demo/base/HelloWorld5.java
+++ src/com/kongzhu/demo/base/HelloWorld5.java
@@ -18,7 +18,7 @@ public class HelloWorld5 {
     public static void main(String[] args) {
         // 声明一个变量，
         // 变量类型是java.lang.String，变量名是s，
-        // 变量的值是Another类中静态方法getMessage的返回值
+        // 变量的值是静态方法getMessage的返回值
         String s = getMessage();
         // 控制台Console打印变量s的值，然后换行
         System.out.println(s);
